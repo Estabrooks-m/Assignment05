b0e5935b763b0200f3adb270c3cc8dd9a573405f
Estabrooks <estabrooks.m@northeastern.edu>
2024-03-04 11:31:21 -0500

    Readme and code outline

---
+++ README.md
@@ -0,0 +1,3 @@
+This project is to get familiar with oop(classes). 
+It will prompt the user with a series of questions and exacute mathmatical operations
+depending on the input from the user. 
---
+++ assignment05_estabrooks.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 10:59:04 2024
+
+@author: Estabrooks
+"""
+class BasicMathOperations:
+    def __init__(self, name, num1, num2, oper, arg):
+        self.name= name
+        self.num1= num1
+        self.num2= num2
+        self.oper=oper
+        self.arg=arg
+    def greet(self, name):
+        
+    def add(self, num1, num2):
+        
+    def performOp(self, num1, num2, oper):
+        
+    def square(self, num1):
+        
+    def factorial(self, num1):
+        
+    def count(self, num1):
+        
+    def hypot(self, num1, num2):
+        
+    def area(self, num1, num2):
+        
+    def power(self, num1):
+        
+    def Type(self, arg):
+        
+def main():
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+main()
+        
