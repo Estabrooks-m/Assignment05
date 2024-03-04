988385cd99796ee9f109fb1377a072a1b19c3750
Estabrooks <estabrooks.m@northeastern.edu>
2024-03-04 18:49:44 -0500

    Added comments to code

--- assignment05_estabrooks.py
+++ assignment05_estabrooks.py
@@ -5,17 +5,22 @@ Created on Mon Mar  4 10:59:04 2024
 @author: Estabrooks
 """
 class BasicMathOperations:
+    #initializes object
     def __init__(self):
         pass
+    
+    #takes in the first and last name and welcomes user with full name
     def greet(self, first, last):
         greet= (f"Welcome {first} {last}")
         return (greet)
-        
+    
+    #adds the two numbers user entered
     def add(self, num1, num2):
         num= num1 + num2
         add= print(num1, "+", num2, "=", num)
         return (add)
-             
+     
+    #depending on operation user chose complete the calculation
     def performOp(self, num1, num2, oper):
         if (oper == "+"):
             num= num1 + num2
@@ -30,11 +35,13 @@ class BasicMathOperations:
             num= num1/num2
             calc= print(num1, "/", num2, "=", num)
         return (calc)
-        
+      
+    #Squares users number
     def square(self, num1):
         num= num1**2
         return(num)        
-        
+     
+    #Finds the factorial of the users number
     def factorial(self, num1):
         num= 1
         for i in range (1, num1+1):
@@ -42,30 +49,34 @@ class BasicMathOperations:
         fact= print(num1, "! =", num)
         return(fact)
             
-        
+    #Starts at 0 and counts up to and including thier number
     def count(self, num1):
         for i in range (0, num1+1):
             count= print (i, "", end= "")
         return(count)
-        
+     
+    #Finds the hypotenuse using pythagorean theorum
     def hypot(self, num1, num2):
         hypot= (num1**2 + num2**2)**.5
         t= round(hypot, 3)
         triangle= print("Hypotenuse= ", t)
         return (triangle)
         
+    #Finds area of a rectangle based on users numbers
     def area(self, num1, num2):
         a= num1 * num2
         area= round (a, 3)
         rect= print("Area =", area)
         return (rect)
         
+    #Finds the power of a number, both chosed by user
     def power(self, num1, num2):
         p= num1 ** num2
         power= round(p, 3)
         num= print(num1, "to the power", num2, "is", power)
         return (num)
-        
+      
+    #Finds the argument type entered by user 
     def Type(self, arg):
         if isinstance(arg, str):
             argument= print(arg, "is a str")
@@ -77,8 +88,7 @@ class BasicMathOperations:
             argument= print("Argument is not a str, int, or float")
             
         return (argument)
-    
-        
+          
 def main():
     #Get user name and welcome them
     first= str(input("Enter your first name "))
@@ -91,9 +101,11 @@ def main():
           "Take a factorial (4), display all number from 0 to your number (5), find a hypotenbuse (6),"
           "Find the area of a rectangle (7), take the power of a numer (8), or find the argument type (9) "))
     
+    #If they pick a number that is not 1-9 show and error and have them try again
     while (pick != 1 and pick != 2 and pick != 3 and pick != 4 and pick != 5 and pick != 6 and pick != 7 and pick !=8 and pick !=9):
         pick= int(input("Invalid input try again "))
 
+    #Based on their choice ask for different variables
     if (pick == 1):
         num1= int(input("Pick a number "))
         num2= int(input("Pick another number "))
@@ -105,6 +117,7 @@ def main():
         num2= float(input("Pick another number "))
         print("You can add (+), subtract (-), multiply (*) or divide (/)")
         op=str((input("Which one do you want to do ")))
+        #If they do not choose a valid operator of *,/,-,+ make them enter again with invalid message
         while (op != "+" and op != "-" and op != "*" and op != "/"):
             op= input("Invalid input try again ")
         send= BasicMathOperations()
@@ -147,9 +160,6 @@ def main():
         arg= BasicMathOperations()
         print(arg.Type(argu))
     
-        
-    end= print("")
-    return(end)        
     
 main()
         
